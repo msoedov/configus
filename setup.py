e0774432917c488399dba6c1b59515eb2fd363d6
Alex Myasoedov <msoedov@gmail.com>
2017-03-14 17:00:53 -0400

    Setup

---
+++ setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = """
+A unified way how to describe a spec of environment paramaters
+required for the application runtime.
+Support override params from command line or env file.
+"""
+
+setup(
+    name="configus",
+    version="0.1.0",
+    description="Configus - validate your env variable configuration",
+    license="MIT",
+    author="Alex Myasoedov",
+    packages=find_packages(),
+    install_requires=['trafaret'],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ]
+)
